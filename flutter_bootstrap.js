5de5cbf3307abe27852397dafbb3b16e83903e1d
VirroHarrou <81008781+VirroHarrou@users.noreply.github.com>
2025-05-27 01:08:57 +0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2150837618"
+    serviceWorkerVersion: "2662054348"
   }
 });
